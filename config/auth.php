93e4290af95b81e836b9a38aa6e00c2e838f9f0b
vinay1237 <53466781+vinay1237@users.noreply.github.com>
2019-12-28 06:54:44 +0530

    cxomm

--- config/auth.php
+++ config/auth.php
@@ -44,15 +44,12 @@ return [
             'driver' => 'session',
             'provider' => 'admins',
         ],
-<<<<<<< HEAD
         'vendor' => [
             'driver' => 'session',
             'provider' => 'vendors',
-=======
         'agent' => [
             'driver' => 'session',
             'provider' => 'agents',
->>>>>>> 43c2a3003781d8f5c59822ce5b0407e844ae63be
         ],
 
         'api' => [
@@ -89,15 +86,12 @@ return [
             'driver' => 'database',
             'table' => 'admin',
         ],
-<<<<<<< HEAD
         'vendors' => [
             'driver' => 'database',
             'table' => 'vendor',
-=======
         'agents' => [
             'driver' => 'database',
             'table' => 'agent',
->>>>>>> 43c2a3003781d8f5c59822ce5b0407e844ae63be
         ],
     ],
 
