5f04a32a48e8fda29c464b164db057ff42cdc050
vin jivani <jivanivinay777@gmail.com>
2020-03-24 01:55:32 +0530

    offer blade change

--- resources/views/web/offers/offer.blade.php
+++ resources/views/web/offers/offer.blade.php
@@ -18,55 +18,85 @@
 @section('content')
 
 <div class="main-content">
-    <div class="row">
-        <div class="col-12">
-            <div class="display-block bg-dark text-white text-center">OFFERS</div>
+    {{-- <div class="min-offer">
+        <div class="row">
+            <div class="col-12">
+                <div class="display-block bg-dark text-white text-center">OFFERS</div>
+            </div>
         </div>
-    </div>
-    <div class="row ml-2 mr-2 mt-3">
-        <div class="col-lg-12 col-sm-12 col-md-12 col-12 col-xs-12">
-            <div class="row">
-                <div class="col-12 col-sm-12 col-lg-4 col-md-6">
-                    <div class="card card-body p-0 pb-1">
-                        <div class="row mt-0 mb-0">
-                            <div class="col-3 col-sm-3 ">
-                                <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
-                            </div>
-                            <div class="col-9 col-lg-7">
-                                <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
-                                <span>Validity : 1 Mar to 31 Apr</span>
+        <div class="row ml-2 mr-2 mt-3">
+            <div class="col-lg-12 col-sm-12 col-md-12 col-12 col-xs-12">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-lg-4 col-md-6">
+                        <div class="card card-body p-0 pb-1">
+                            <div class="row mt-0 mb-0">
+                                <div class="col-3 col-sm-3 ">
+                                    <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
+                                </div>
+                                <div class="col-9 col-lg-7">
+                                    <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
+                                    <span >Validity : 1 Mar to 31 Apr</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
+                    <div class="col-12 col-sm-12 col-lg-4 col-md-6">
+                        <div class="card card-body p-0 pb-1">
+                            <div class="row mt-0 mb-0">
+                                <div class="col-3 col-sm-3 ">
+                                    <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
+                                </div>
+                                <div class="col-9 col-lg-7">
+                                    <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
+                                    <span>Validity : 1 Mar to 31 Apr</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-12 col-sm-12 col-lg-4 col-md-6">
+                        <div class="card card-body p-0 pb-1">
+                            <div class="row mt-0 mb-0">
+                                <div class="col-3 col-sm-3 ">
+                                    <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
+                                </div>
+                                <div class="col-9 col-lg-7">
+                                    <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
+                                    <span>Validity : 1 Mar to 31 Apr</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div class="col-12 col-sm-12 col-lg-4 col-md-6">
-                    <div class="card card-body p-0 pb-1">
-                        <div class="row mt-0 mb-0">
-                            <div class="col-3 col-sm-3 ">
-                                <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
-                            </div>
-                            <div class="col-9 col-lg-7">
-                                <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
-                                <span>Validity : 1 Mar to 31 Apr</span>
-                            </div>
-                        </div>
-                    </div>
-
+            </div>
+        </div>
+    </div> --}}
+    <div class="max-offer">
+        <div class="container ">
+            <div class="row mt-5">
+                <div class="col-12">
+                    <div class="display-block   text-center" style="font-size:35px">OFFERS</div>
                 </div>
-                <div class="col-12 col-sm-12 col-lg-4 col-md-6">
-                    <div class="card card-body p-0 pb-1">
-                        <div class="row mt-0 mb-0">
-                            <div class="col-3 col-sm-3 ">
-                                <img src="{{ asset('web\images\redbus\offer\100x100.png') }}" style="width:50px;height:50px" alt="" class="mx-auto">
-                            </div>
-                            <div class="col-9 col-lg-7">
-                                <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
-                                <span>Validity : 1 Mar to 31 Apr</span>
+            </div>
+            <div class="row ">
+                <div class="col-lg-12 col-sm-12 col-md-12 col-12 col-xs-12">
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-lg-4 col-md-6">
+                            <div class="card card-body p-0 pb-1">
+                                <div class="row mt-0 mb-0">
+                                    <div class="col-12 col-sm-12  mx-auto text-center">
+                                        <img src="{{ asset('web\images\redbus\offer\274x274.png') }}" style="width:100px;height:100px" alt="" class="mx-auto">
+                                    </div>
+                                    <div class="col-9 col-lg-7">
+                                        <h6 class="text-danger">Save up to Rs 250 on bus ticket</h5>
+                                        <span >Validity : 1 Mar to 31 Apr</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -86,7 +116,7 @@
 @section('after-js')
 
 
- 
+
 
 @endsection
 
